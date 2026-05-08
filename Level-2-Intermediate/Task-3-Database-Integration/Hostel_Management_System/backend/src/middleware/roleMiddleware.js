a64811266f859f23803dede1eb179ae54d0ba7d7
Dinesh Jagadale <dineshkjagadale12@gmail.com>
2026-05-08 23:44:25 +0530

    save files

--- Level-2-Intermediate/Task-3-Database-Integration/Hostel_Management_System/backend/src/middleware/roleMiddleware.js
+++
@@ -1,6 +0,0 @@
-//Hostel_Management_System\backend\src\middleware\roleMiddleware.js
-export const rectorOnly = (req, res, next) => {
-  if (req.user.role !== "rector")
-    return res.status(403).json({ message: "Rector only" });
-  next();
-};
